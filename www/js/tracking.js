3f664d9fbfc8a92fbde9638b780e03db9ff0ac97
ahrobinson <ahrobins3@gmail.com>
2015-11-19 16:17:08 -0600

    integrate master with tacking.js, needs code for sounding alarm

--- www/js/tracking.js
+++ www/js/tracking.js
@@ -4,16 +4,18 @@ angular.module('trackingModule', []).
       isWithinRadius(alertRadius, next, endCoords, startCoords);
     };
   }]).
-  factory('distance', function(lat1, lon1, lat2, lon2) {
-    var p = 0.017453292519943295;    // Math.PI / 180
-    var c = Math.cos;
-    var a = 0.5 - c((lat2 - lat1) * p)/2 +
-            c(lat1 * p) * c(lat2 * p) *
-            (1 - c((lon2 - lon1) * p))/2;
-    var dist = 2 * 3959 * Math.asin(Math.sqrt(a)); // 2 * R; R = 3959 miles
-    return (1.9 * dist > 2) ? dist : dist * 1.9;
+  factory('distance', function() {
+    return function(lat1, lon1, lat2, lon2){
+      var p = 0.017453292519943295;    // Math.PI / 180
+      var c = Math.cos;
+      var a = 0.5 - c((lat2 - lat1) * p)/2 +
+              c(lat1 * p) * c(lat2 * p) *
+              (1 - c((lon2 - lon1) * p))/2;
+      var dist = 2 * 3959 * Math.asin(Math.sqrt(a)); // 2 * R; R = 3959 miles
+      return (1.9 * dist > 2) ? dist : dist * 1.9;
+    }
   }).
-  factory('isWithinRadius', ['$scope', 'distance', function($scope, distance) {
+  factory('isWithinRadius', ['distance', function(distance) {
     var usingGeolocation = false;
     return function(alertRadius, endCoords, startCoords, next) {
       var callback = function(startCoords) {
